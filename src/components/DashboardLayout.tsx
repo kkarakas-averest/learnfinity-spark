2fe4813b0a0ae48071ef6b9b3b89e647eb9619eb
Kubilay Karakas <kkarakas@averesttraining.com>
2025-03-05 13:27:15 +0300

    Fix TypeScript errors: Add tsconfig.local.json files and update React imports

--- src/components/DashboardLayout.tsx
+++ src/components/DashboardLayout.tsx
@@ -1,5 +1,4 @@
-
-import * as React from "react";
+import React from "react";
 import { Link, useNavigate, useLocation } from "react-router-dom";
 import { 
   BarChart2, 
--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -1,4 +1,4 @@
-import * as React from "react";
+import React from "react";
 import { cn } from "@/lib/utils";
 import { ArrowRight } from "lucide-react";
 import { Button } from "@/components/ui/button";
