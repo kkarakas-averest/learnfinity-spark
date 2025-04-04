5aa52bd6a3a49cc28e8ed1aa15fa716fcee36def
Kubilay Karakas <kkarakas@averesttraining.com>
2025-04-04 09:18:20 +0300

    chore: Add .vite directory to .gitignore

--- .vite/deps/react-dom.js
+++
@@ -1,7 +0,0 @@
-import {
-  require_react_dom
-} from "./chunk-RGVYSSFY.js";
-import "./chunk-QZ55VL3A.js";
-import "./chunk-EWTE5DHJ.js";
-export default require_react_dom();
-//# sourceMappingURL=react-dom.js.map
